7202394e79d9a8da2d15db9e0ce3d576501428c4
Pascal Zumkehr <spam@codez.ch>
2023-07-02 01:11:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Infinislide
+
+Display pictures infinitely in random order.
+
+To run locally, run a simple webserver. E.g.
+
+```sh
+browser-sync start -s -f . --no-notify --host localhost --port 9000
+```
+
+MIT License
+Copyright (c) 2023 code!z
---
+++ assets/infinislide.js
@@ -0,0 +1,56 @@
+const duration = 8; // seconds
+
+const indexPath = "images_test/index.txt";
+const imagesPath = "images_test/";
+
+Array.prototype.randomize = function () {
+  var i = this.length,
+    j,
+    temp;
+  if (i > 1 && (i > 2 || Math.random() > 0.5)) {
+    while (--i) {
+      j = Math.floor(Math.random() * i);
+      temp = this[i];
+      this[i] = this[j];
+      this[j] = temp;
+    }
+  }
+};
+
+const reloadImages = async function () {
+  console.log("reload");
+  const response = await fetch(indexPath);
+  const list = await response.text();
+  const images = list.split("\n").filter((line) => line.trim().length > 0);
+  displayImages(images);
+};
+
+const displayImages = function (images) {
+  images.randomize();
+  preloadImages(images);
+  iterateImages(images, 0);
+};
+
+const iterateImages = function (images, i) {
+  if (i < images.length) {
+    showImage(images[i]);
+    setTimeout(() => iterateImages(images, i + 1), duration * 1000);
+  } else {
+    reloadImages();
+  }
+};
+
+const preloadImages = function (images) {
+  images.forEach((image) => {
+    var img = new Image();
+    img.src = imagesPath + image;
+  });
+};
+
+const showImage = function (image) {
+  document.getElementById("image").setAttribute("src", imagesPath + image);
+};
+
+document.addEventListener("DOMContentLoaded", (_) => reloadImages());
+
+console.log("infini");
